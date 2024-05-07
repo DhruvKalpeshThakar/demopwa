a7a6f99759e72faaa1cf39ff3f225db74eb2d128
Dhruv <dhruvthakar2210@gmail.com>
2024-05-07 12:36:38 +0530

    Updated Service worker

--- public/sw.js
+++ public/sw.js
@@ -1,49 +1,24 @@
-const CACHE_NAME = "version-1";
-const urlsTOCache = [
+let cacheData = "version-1";
+let urlsTOCache = [
   "/",
   "/index.html",
   "/offline.html",
   "/manifest.json",
   "/static/js/bundle.js",
-  // Add other assets and routes to cache
 ];
 
-const self = this;
-
-// install server worker
-self.addEventListener("install", (event) => {
+this.addEventListener("install",(event) =>{
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log("opened cache");
-      return cache.addAll(urlsTOCache);
+    caches.open(cacheData).then((cache) =>{
+      cache.addAll(urlsTOCache)
     })
-  );
-});
+  )
+})
 
-// listen to requests
-self.addEventListener("fetch", (event) => {
+this.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then(() => {
-      return fetch(event.request).catch(() => caches.match("./offline.html"));
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
-  );
-});
-
-// Activate the service worker
-
-self.addEventListener("activate", (event) => {
-  const cacheWhileList = [];
-  cacheWhileList.push(CACHE_NAME);
-
-  event.waitUntil(
-    caches.keys().then((cacheNames) =>
-      Promise.all(
-        cacheNames.map((cacheName) => {
-          if (!cacheWhileList.includes(cacheName)) {
-            return caches.delete(cacheName);
-          }
-        })
-      )
-    )
   );
 });

d785884d7b62c3060e6e4d4a1878479335230169
Dhruv <dhruvthakar2210@gmail.com>
2024-05-07 14:49:29 +0530

    Added Local Storage Access and ALert on Offline Mode

--- public/sw.js
+++ public/sw.js
@@ -5,20 +5,33 @@ let urlsTOCache = [
   "/offline.html",
   "/manifest.json",
   "/static/js/bundle.js",
+  "/users",
+  "/logo192.png",
+  "/favicon.ico",
 ];
 
-this.addEventListener("install",(event) =>{
+this.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(cacheData).then((cache) =>{
+    caches.open(cacheData).then((cache) => {
       cache.addAll(urlsTOCache)
     })
   )
 })
 
 this.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+  if (!navigator.onLine) {
+
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        
+        return response || fetch(event.request);
+
+
+
+        // let requestUrl = event.request.clone();
+        // return fetch(requestUrl);
+      })
+    );
+  }
+
 });
--- src/Users.js
+++ src/Users.js
@@ -1,9 +1,78 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Table } from 'react-bootstrap';
 
 export function Users() {
+
+    const [data, setData] = useState([])
+    const [mode, setmode] = useState("online")
+
+    useEffect(() => {
+        let url = 'https://jsonplaceholder.typicode.com/users'
+        fetch(url).then((response) => {
+            response.json().then((result) => {
+                console.warn("result", result)
+                setData(result)
+
+                //To set Api Data in Local Storage
+                localStorage.setItem('users', JSON.stringify(result))
+
+            })
+        }).catch(e => {
+            let collection = localStorage.getItem("users")
+            setData(JSON.parse(collection))
+            setmode('Offline')
+            // alert("Catch Block")
+        })
+    }, [])
+
+
     return (
         <div>
-            Welcome to the Users Page
+            {mode === 'Offline' ?
+                <div className='alert alert-warning' role='alert'>You are in Offline Mode </div>
+                : null
+            }
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Username</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map((item) => (
+                        <tr>
+                            <td>{item.id}</td>
+                            <td>{item.name}</td>
+                            <td>{item.username}</td>
+                            <td>{item.email}</td>
+                        </tr>
+                    ))
+                    }
+                    {/* <tr>
+                        <td>1</td>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td colSpan={2}>Larry the Bird</td>
+                        <td>@twitter</td>
+                    </tr> */}
+                </tbody>
+            </Table>
         </div>
     )
 }
+
+
+
